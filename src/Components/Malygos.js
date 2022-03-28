02dddc6986b6211561c5a5c2f94d70cff2771c0b
Jay Nguyen <jnguyen0291@gmail.com>
2022-03-27 17:45:02 -0700

    React setup, added portfolio images.

---
+++ src/Components/Malygos.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Malygos() {
+	return (
+		<>
+			<img
+				src={require('../Malygos.jpeg')}
+				className='PortfolioImage'
+				alt="Malygos, Jay's dog."
+				width='250'
+			/>
+			<br></br>
+		</>
+	);
+}
---
+++ src/Components/Onyxia.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Onyxia() {
+	return (
+		<>
+			<img
+				src={require('../Onyxia.jpeg')}
+				className='PortfolioImage'
+				alt="Onyxia, Jay's cat."
+				width='250'
+			/>
+			<br></br>
+		</>
+	);
+}
