a45aa9df48ce5002f3f9a1cb7983d8fd94707e59
Jay Nguyen <jnguyen0291@gmail.com>
2022-03-29 17:55:49 -0700

    Added Bootstrap framework; removed Malygos and Onyxia image components; used Bootstrap to add images via Carousel

--- src/App.js
+++ src/App.js
@@ -1,15 +1,43 @@
 import './App.css';
+import { Container, Carousel } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './Components/AboutMe';
-import Onyxia from './Components/Onyxia';
-import Malygos from './Components/Malygos';
 
 function App() {
 	return (
-		<>
+		<Container>
+			<Carousel>
+				<div className='carousel-item'>
+					<img
+						className='d-block w-100'
+						src={require('./Malygos.jpeg')}
+						alt="Jay's husky, Malygos."
+					/>
+					<Carousel.Caption>
+						<h3>Jay's husky, Malygos!</h3>
+						<p>
+							Malygos is the sweetest, friendliest boy and loves trips to the
+							dog park.
+						</p>
+					</Carousel.Caption>
+				</div>
+				<div className='carousel-item'>
+					<img
+						className='d-block w-100'
+						src={require('./Onyxia.jpeg')}
+						alt="Jay's tortoiseshell cat, Onyxia."
+					/>
+					<Carousel.Caption>
+						<h3>Jay's tortoiseshell cat, Onyxia!</h3>
+						<p>
+							Onyxia can be a little Dr Jekyll and Mr Hyde. She's just as likely
+							to cuddle you as she is to pounce you!
+						</p>
+					</Carousel.Caption>
+				</div>
+			</Carousel>
 			<AboutMe />
-            <Onyxia />
-			<Malygos />
-		</>
+		</Container>
 	);
 }
 
