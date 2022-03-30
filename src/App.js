0da788a0fe9ae8b883ea2609bbaf7149dbec6eb6
Jay Nguyen <jnguyen0291@gmail.com>
2022-03-29 18:59:52 -0700

    CSS attempts on carousel-caption

--- src/App.js
+++ src/App.js
@@ -7,34 +7,38 @@ function App() {
 	return (
 		<Container>
 			<Carousel>
-				<div className='carousel-item'>
+				<Carousel.Item>
+					<div className='carousel-caption' id='above'>
+						<h3 className='text'>Jay's husky, Malygos!</h3>
+					</div>
 					<img
 						className='d-block w-100'
 						src={require('./Malygos.jpeg')}
 						alt="Jay's husky, Malygos."
 					/>
-					<Carousel.Caption>
-						<h3>Jay's husky, Malygos!</h3>
-						<p>
+					<div className='carousel-caption'>
+						<p className='text'>
 							Malygos is the sweetest, friendliest boy and loves trips to the
 							dog park.
 						</p>
-					</Carousel.Caption>
-				</div>
-				<div className='carousel-item'>
+					</div>
+				</Carousel.Item>
+				<Carousel.Item>
+					<div className='carousel-caption' id='above'>
+						<h3 className='text'>Jay's cat, Onyxia!</h3>
+					</div>
 					<img
 						className='d-block w-100'
 						src={require('./Onyxia.jpeg')}
 						alt="Jay's tortoiseshell cat, Onyxia."
 					/>
-					<Carousel.Caption>
-						<h3>Jay's tortoiseshell cat, Onyxia!</h3>
-						<p>
-							Onyxia can be a little Dr Jekyll and Mr Hyde. She's just as likely
-							to cuddle you as she is to pounce you!
+					<div className='carousel-caption'>
+						<p className='text'>
+							Onyxia can be a little Dr. Jekyll and Mr. Hyde. She's just as
+							likely to cuddle you as she is to pounce you!
 						</p>
-					</Carousel.Caption>
-				</div>
+					</div>
+				</Carousel.Item>
 			</Carousel>
 			<AboutMe />
 		</Container>
