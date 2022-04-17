700153dc8074c0b501dc877ba5b1648fd3017d89
Jay Nguyen <jnguyen0291@gmail.com>
2022-04-16 20:33:33 -0700

    Removed carousel; added TopNav component

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import './App.css';
-import { Container, Carousel } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AboutMe from './Components/AboutMe';
+import TopNav from './Components/TopNav';
 
 function App() {
 	return (
 		<Container fluid>
-			<Carousel className='carousel'>
-				<Carousel.Item>
-					<img
-						id='profileImages'
-						className='d-block w-100'
-						src={require('./Malygos.jpeg')}
-						alt="Jay's husky, Malygos."
-					/>
-					<Carousel.Caption>
-						<h3 className='text'>Jay's husky, Malygos!</h3>
-						<p className='text'>
-							Malygos is the sweetest, friendliest boy and loves trips to the
-							dog park.
-						</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						id='profileImages'
-						className='d-block w-100'
-						src={require('./Onyxia.jpeg')}
-						alt="Jay's tortoiseshell cat, Onyxia."
-					/>
-					<Carousel.Caption>
-						<h3 className='text'>Jay's cat, Onyxia!</h3>
-						<p className='text'>
-							Onyxia is as likely to cuddle you as she is to pounce you.
-						</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-				<Carousel.Item>
-					<img
-						id='profileImages'
-						className='d-block w-100'
-						src={require('./Jay.jpeg')}
-						alt='Jay Nguyen'
-					/>
-					<Carousel.Caption>
-						<p className='text'>
-							And me, Jay Nguyen. Your future web developer!
-						</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-			</Carousel>
+			<TopNav />
 			<AboutMe />
 		</Container>
 	);
